fc59143b071f72e88c62db63e0c05285a71d496a
Anthony Hams <anthony.hams@atradius.com>
2018-11-19 15:51:25 +0000

    Initial commit

---
+++ monteCarlo.R
@@ -0,0 +1,19 @@
+
+
+pi_mc1 <- function(samples=10) {
+  x <- runif(samples)
+  y <- runif(samples)
+  
+  return(4.0*sum((x^2 + y^2) < 1.0)/samples)
+}
+
+pi_mc1(1000000)
+gc()
+
+x <- seq(8)
+dim(x) <- c(2,2,2)
+
+x
+
+x[1,2,1]
+
---
+++ workScript.R
@@ -0,0 +1,33 @@
+a <- 1
+
+f <- function(x) {
+  return (x^2)
+}
+
+f(1)
+
+round(1.5)
+round(2.500001)
+
+
+l <- list(name="Jane", age=3, grades = c(4,56,6,3))
+
+str(l) 
+
+df <- data.frame(a=c(1,2,3), b = c("A", "B", "C")) ; df
+
+str(df)
+
+str(mtcars)
+
+View(iris)
+
+
+kml <- mtcars$mpg * 0.425144
+
+mtcars$kml <- kml
+
+mtcars
+
+
+
